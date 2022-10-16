25b0e8509aebf813084f274c63d88409568fb1a2
shruti patl <shrutipatil11823@gmail.com>
2022-10-16 14:35:47 +0530

    First C2TCProject CoreJava Commit

---
+++ C2TCProject/src/com/cg/corejava/FirstProgram.java
@@ -0,0 +1,11 @@
+package com.cg.corejava;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hii,I Love My India!");    //Used System Class
+		
+	}
+
+}
